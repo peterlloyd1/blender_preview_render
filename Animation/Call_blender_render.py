48769d5f45045c5c782d99313825a33b0b7c497b
peterlloyd1 <peter@zudrit.com>
2021-08-10 13:16:47 +0300

    Looping trough list
    Looping trough folder and creating a list with only the blend files.

--- Animation/Call_blender_render.py
+++ Animation/Call_blender_render.py
@@ -1,17 +1,31 @@
 # Task:
 # Somehow call all files in folder and save them as path
 # then call them as blender for render, when one is rendered, render the next.
-#
+
 
 import os
+import subprocess
 
 # Grab all files from current directory
-files = os.listdir('.')
+files = os.listdir(".")
+
 blend_files = []
 
-# filter trough current folder and check if the name ".blend " is in there. ASD
+# filter trough current folder and check if the name ".blend " is in there.
 for file in files:
     if ".blend" in file:
-        blend_files.append(file)
+        if file[-1] == "1":
+            print("deleted " + file)
+        else:
+            blend_files.append(file)
+
+        # Need remove all files that end with .blend1 somehow
+
+print("blender " + blend_files[0] + " --python Blender_Render.py")
+
+
+# subprocess.call(
+#    "blender " + blend_files[0] + " --python Blender_Render.py", shell=True)
+
 
 print(blend_files)
